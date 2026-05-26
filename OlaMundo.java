deaa4ffadd0b570886309167d4b07d348d943cb3
Rodrigo Costa Santos <rodrigoscsantos@live.com>
2026-05-26 20:31:49 -0300

    Entrada do nome pelo terminal

--- OlaMundo.java
+++ OlaMundo.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class OlaMundo {
     public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
         System.out.println("Olá, mundo!");
+        System.out.println("Digite o nome da pessoa: ");
+        String nomePessoa = scn.nextLine();
+        System.out.println("O nome da pessoa é: " + nomePessoa);
+        scn.close();
     }
 }
