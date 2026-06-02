3c2a80713dea28b68c48087e5148d860b41a3e7f
Rodrigo Costa Santos <rodrigoscsantos@live.com>
2026-06-02 14:44:05 -0300

    iniciando o programa

---
+++ Questao2.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Questao2 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("\nFaturamento de sanemento básico\n");
+        
+        scn.close();
+    }
+}
