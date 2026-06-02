ab7dcc7551dc88ecb59ad2dc3e1ec71aa44aff44
Rodrigo Costa Santos <rodrigoscsantos@live.com>
2026-06-02 14:45:56 -0300

    variavel volume consumido

--- Questao2.java
+++ Questao2.java
@@ -4,6 +4,8 @@ public class Questao2 {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         System.out.println("\nFaturamento de sanemento básico\n");
+        System.out.print("\nForneça o volume consumido em m³ ");
+        double volumeConsumido = scn.nextDouble();
         
         scn.close();
     }
