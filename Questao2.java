fc305508a9bbcf08802fe43b25a226ebeb6823bd
Rodrigo Costa Santos <rodrigoscsantos@live.com>
2026-06-02 14:55:37 -0300

    organizando o codigo

--- Questao2.java
+++ Questao2.java
@@ -4,12 +4,16 @@ public class Questao2 {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         System.out.println("\nFaturamento de sanemento básico\n");
+        
         System.out.print("\nForneça o volume consumido em m³ ");
         double volumeConsumido = scn.nextDouble();
+        
         System.out.printf("\nForneça o valor do m³ R$ ");
         double valorVolume = scn.nextDouble();
+        
         System.out.printf("\nForneça o valor da tarifa fixa R$ ");
         double tarifaFixa = scn.nextDouble();
+        
         System.out.printf("\nForneça o percentual da taxa de esgoto ");
         double txEsgoto = scn.nextDouble();
         scn.close();
