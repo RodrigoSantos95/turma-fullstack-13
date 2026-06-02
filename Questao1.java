1865d75f6429cb316b2258a4b74c940822ce8352
Rodrigo Costa Santos <rodrigoscsantos@live.com>
2026-06-02 14:31:10 -0300

    add variavel qtdHoras

--- Questao1.java
+++ Questao1.java
@@ -4,5 +4,8 @@ public class Questao1 {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         System.out.println("\nFolha de pagamento\n");
+        System.out.println("\nDigite a quantidade de horas trabalhadas no mês: ");
+        double qdtHoras = scn.nextDouble();
+        scn.close();
     }
 }
