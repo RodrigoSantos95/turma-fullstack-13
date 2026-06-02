814e6b281c5d71097ad6c04cda82bff044d89e59
Rodrigo Costa Santos <rodrigoscsantos@live.com>
2026-06-02 12:41:59 -0300

    inicio do codigo

---
+++ Questao1.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Questao1 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("\nFolha de pagamento\n");
+    }
+}
