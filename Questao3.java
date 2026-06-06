dfcf5ceea177808837981f9b0f614bd8f8a3f330
Rodrigo Costa Santos <rodrigoscsantos@live.com>
2026-06-06 14:43:35 -0300

    organizacao parcial codigo

--- Questao3.java
+++ Questao3.java
@@ -4,12 +4,16 @@ public class Questao3 {
     public static void main(String[] args) {
         Scanner scn = new Scanner (System.in);
         System.out.print("\nConversão de moeda\n");
+        
         System.out.print("\nDigite o valor desejado a ser comprado em dólar (USD) $ ");
         double valorDolar = scn.nextDouble();
+        
         System.out.print("Digite a cotação oficial atual do dólar ");
         double cotacaoDolar = scn.nextDouble();
+        
         System.out.print("Digite a taxa de spread do banco ");
         double txSpread = scn.nextDouble();
+        
         scn.close();    
     }
 }
